2b02c4ce0f8829c41865c4ef8f3152f4fc92eeaa
Jessan Palban <106059541+San103@users.noreply.github.com>
2024-02-17 19:15:16 +0800

    Update setTime.sh

--- setTime.sh
+++ setTime.sh
@@ -3,6 +3,8 @@
 # Fetch the current date and time from an online source
 online_date=$(curl -s http://worldtimeapi.org/api/timezone/Asia/Manila | jq -r .utc_datetime)
 
+#change the utc.datetime to datetime if its not showing the right date
+
 # Set the system date and time
 sudo date -s "$online_date"
 
